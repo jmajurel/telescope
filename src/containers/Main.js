e9420d4cbda9ab3ff806973655d4064c912d5616
jmajurel <jeanfelix.majurel@gmail.com>
2021-12-30 19:14:45 +0100

    added lazy loading

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import HomePage from '../components/HomePage';
-import Asteroids from '../containers/Asteroids';
-import SpaceWeather from '../containers/SpaceWeather';
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+
+import HomePage from "../components/HomePage";
+import Asteroids from "../containers/Asteroids";
+import SpaceWeather from "../containers/SpaceWeather";
 
 class Main extends Component {
-  render(){
+  render() {
     return (
       <Switch>
-        <Route exact path='/' component={HomePage}/> 
-        <Route exact path='/asteroids' component={Asteroids}/> 
-	<Route exact path='/weather' component={SpaceWeather} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/asteroids" component={Asteroids} />
+        <Route exact path="/weather" component={SpaceWeather} />
       </Switch>
     );
   }
